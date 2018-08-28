6614e7285411b255652842fca168af4987e84cc1
Unknown <luton66@gmail.com>
2018-08-28 23:43:44 +0100

    Rest Controller Update
    Added the Rest Details to return all Anime

--- src/main/java/com/fecklefacemassive/www/education/MongoApp/RestControllers/AnimeDetailsRest.java
+++ src/main/java/com/fecklefacemassive/www/education/MongoApp/RestControllers/AnimeDetailsRest.java
@@ -1,4 +1,29 @@
 package com.fecklefacemassive.www.education.MongoApp.RestControllers;
 
+import com.fecklefacemassive.www.education.MongoApp.Dao.AnimeBaseRepository;
+import com.fecklefacemassive.www.education.MongoApp.pojo.AnimeDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/anime")
+
 public class AnimeDetailsRest {
+
+    private AnimeBaseRepository animeBaseRepository;
+
+    public AnimeDetailsRest(AnimeBaseRepository animeBaseRepository) {
+        this.animeBaseRepository = animeBaseRepository;
+    }
+
+    @GetMapping("/")
+    public List<AnimeDetails> displayAll() {
+        List<AnimeDetails> allAnime = animeBaseRepository.findAll();
+
+        return allAnime;
+    }
+
 }
